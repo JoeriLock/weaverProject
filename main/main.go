4d62f4ddc59840569b1f63f8c26cbed4081e49fd
joeri <joeri.lock@live.nl>
2021-03-21 15:22:22 +0100

    read from csv

--- main/main.go
+++ main/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
-	"joeri/weave/energy"
 	"joeri/weave/meter"
 )
 
 func main() {
-	test := energy.EnergyToCost(100, true, true, true)
-	fmt.Println(test)
-	val := meter.GetRow()
-	fmt.Println(val)
-
+	// test := energy.EnergyToCost(100, true, true, true)
+	// fmt.Println(test)
+	// val := meter.GetRow()
+	// fmt.Println(val)
+	// fmt.Println(meterdata.GetId())
+	// a := []int{1, 2, 3}
+	// fmt.Println(a[1:])
+	val := meter.GetData()
+	for _, row := range val {
+		fmt.Println(row)
+	}
 }
